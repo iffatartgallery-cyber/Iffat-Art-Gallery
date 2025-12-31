f82de6bc625966f0a0dff7a8e63045130a2826f2
iffatartgallery-cyber <iffatartgallery@gmail.com>
2025-12-31 19:14:50 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -19,7 +19,8 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  outputFileTracingRoot: path.resolve(__dirname, "../../"),
+  
+  outputFileTracingRoot: path.resolve(__dirname),
   typescript: {
     ignoreBuildErrors: true,
   },
